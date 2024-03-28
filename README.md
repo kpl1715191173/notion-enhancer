5ecfde95c9a4d835e9e336d8e4389ff60a454a29
Alan Lee <1715191173@qq.com>
2024-03-28 11:14:37 +0800

    revert: revert to the latest prod version

--- README.md
+++
@@ -1,5 +0,0 @@
-# notion-enhancer/desktop
-
-an enhancer/customiser for the all-in-one productivity workspace notion.so (app)
-
-[read the docs online](https://notion-enhancer.github.io/)
--- scripts/build-browser-extension.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-version=$(node -p "require('./package.json').version")
-
-cd src
-mkdir -p ../dist
-rm -f "../dist/notion-enhancer-$version.zip"
-zip -r9 "../dist/notion-enhancer-$version.zip" .
