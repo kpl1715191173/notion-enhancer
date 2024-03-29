07aab9b12dd5bb3169f1cdc91300431205300658
Alan Lee <1715191173@qq.com>
2024-03-29 17:11:00 +0800

    feat: add README file

--- cmd/start.bat
+++ cmd/start.bat
@@ -1,6 +1,7 @@
 @echo off
 REM 设定NVM路径
 SET PATH=D:/nvm/v18.20.0;%PATH%
+REM 设定Notion的app.asar位置
 SET notion_path="C:/Users/X/AppData/Local/Programs/Notion Enhanced/resources"
 
 REM 需要先安装nvm -> node 8 18
