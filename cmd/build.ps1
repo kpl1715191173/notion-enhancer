2e45507aaf9d3df1d917cbeef3407f5ed5c32275
Alan Lee <1715191173@qq.com>
2024-04-01 11:39:42 +0800

    refactor: build powershell

--- cmd/build.ps1
+++ cmd/build.ps1
@@ -1,5 +1,11 @@
+Write-Output "Executing now"
+
 # git 忽略对配置文件的修改
 git update-index --assume-unchanged var.json
+
+# 获取当前运行路径
+$script_dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Set-Location $script_dir
 
 # 读取 JSON 文件内容
 $jsonContent = Get-Content -Raw -Path "./var.json" | ConvertFrom-Json
@@ -14,6 +20,8 @@ asar pack ./notion-enhancer ./app.asar --unpack ./notion-enhancer/cmd, ./notion-
 Copy-Item app.asar $($jsonContent.notion_enhancer_path) # $($jsonContent.$key) 获取Json变量
 
 # 删除工作区下临时生成的app.asar
-Remove-Item -Path "app.asar"
+#Remove-Item -Path "app.asar"
+
+Clear-Host
 
 Write-Output "Build finished~ ^_^"
--- cmd/inject.ps1
+++ cmd/inject.ps1
@@ -2,12 +2,27 @@
 # 将 notion-enhancer 库复制到 node_module
 # 目前不考虑 npm i notion-enhancer 是因为防止其他库的版本改动报错
 
+# 获取当前运行路径
+$script_dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Set-Location $script_dir
+
 # 以项目根目录为基准
 Set-Location ../
 
 # 定义源文件夹和目标文件夹
 $source = ".\shared\notion-enhancer"
 $destination = ".\node_modules"
+$notion_enhancer_destination = $destination + "\notion-enhancer"
+
+# 先删除 node_modules\notion-enhancer
+try {
+    Remove-Item $notion_enhancer_destination -Recurse -Force
+}
+catch {
+    Write-Output "No Such Dictionary, it doesn't matter. Let's move on~"
+}
 
 # 复制文件夹及其内容，保持完整目录结构
 Copy-Item -Path $source -Destination $destination -Recurse -Container -Force
+
+Write-Output "Inject Successfully, next you should run build."
